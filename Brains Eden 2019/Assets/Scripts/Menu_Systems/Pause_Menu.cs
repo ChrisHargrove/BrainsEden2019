b44b6a6008f9ea22265de04394fd416a781be18c
Sam <chandler27s@yahoo.com>
2019-06-28 22:04:16 +0100

    Added pause system (Ctrl is the pause button)
    Pausing the game will enable or disable the pause game canvas and will stop physics

---
+++ Brains Eden 2019/Assets/Scripts/Menu_Systems/Pause_Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Menu : MonoBehaviour
+{
+    private bool is_paused = false;
+    [SerializeField] private Canvas pause_canvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            is_paused = !is_paused;
+        }
+
+        if (is_paused)
+        {
+            pause_canvas.gameObject.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            pause_canvas.gameObject.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+}
