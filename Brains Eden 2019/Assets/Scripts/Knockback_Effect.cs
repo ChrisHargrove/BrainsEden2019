c20a5d914d7a430841ca499368d5603d652a155a
Sam <chandler27s@yahoo.com>
2019-06-28 20:32:24 +0100

    Added knockback script to allow objects to be knocked back by spells.

---
+++ Brains Eden 2019/Assets/Scripts/Knockback_Effect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback_Effect : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float knockback_force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Spell")
+        {
+            if (rb != null)
+            {
+                Vector3 force_dir = (transform.position - other.transform.position).normalized * knockback_force;
+                rb.AddForce(force_dir);
+            }
+
+        }
+    }
+}
