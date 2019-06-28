6b74ccfb26b00d8df997d32b8bc79abed44b1b35
Sam <chandler27s@yahoo.com>
2019-06-28 17:48:40 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Brains Eden 2019/Assets/Scripts/Player_Controls.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controls : MonoBehaviour
+{
+    CharacterController char_con;
+
+    [SerializeField] float move_speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        char_con = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            char_con.Move(transform.forward * Time.deltaTime * move_speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            char_con.Move(-(transform.forward * Time.deltaTime * move_speed));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            char_con.Move(transform.right * Time.deltaTime * move_speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            char_con.Move(-(transform.right * Time.deltaTime * move_speed));
+        }
+    }
+}
