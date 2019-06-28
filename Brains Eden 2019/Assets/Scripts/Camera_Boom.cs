fb0f9fe56d22f0ec015cd4e1de3bb366688fa010
Sam <chandler27s@yahoo.com>
2019-06-28 20:17:03 +0100

    Added camera boom and removed camera from player prefab

---
+++ Brains Eden 2019/Assets/Scripts/Camera_Boom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Boom : MonoBehaviour
+{
+    [SerializeField] private GameObject target_object;
+    [SerializeField] private float camera_distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(target_object.transform.position, -((target_object.transform.forward*2) - target_object.transform.up), out hit, 20.0f);
+
+        float current_distance = camera_distance;
+        if (hit.collider != null)
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = target_object.transform.position + -((target_object.transform.forward * 2) - target_object.transform.up) * camera_distance;
+        }
+
+        transform.LookAt(target_object.transform);
+
+        
+    }
+}
