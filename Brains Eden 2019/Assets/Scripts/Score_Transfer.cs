e2d3e4611622f7e1315b2e0a41339ded82246e4e
Sam <chandler27s@yahoo.com>
2019-06-28 23:16:33 +0100

    Added score transfer script (Currently in progress)

---
+++ Brains Eden 2019/Assets/Scripts/Score_Transfer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score_Transfer : MonoBehaviour
+{
+    [SerializeField] public int player_score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
