81559fb68fc9e3e5f5569a0cad68503afa6e174b
Chris <hargrove.chris91@gmail.com>
2019-06-28 18:40:22 +0100

    Created Basic Enemy
    Has NavMesh Agent and RigidBody Components, and some basic GoTo commands.

---
+++ Brains Eden 2019/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+    public Rigidbody RigidBody;
+
+
+    public void GoTo(Transform transform) {
+        Agent.SetDestination(transform.position);
+    }
+
+    public void GoTo(Vector3 location) {
+        Agent.SetDestination(location);
+    }
+}
