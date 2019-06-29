d47b8cdea0e1f39785546d62b317aeec4de3ad3e
Sam <chandler27s@yahoo.com>
2019-06-29 23:20:56 +0100

    Began Tutorial stuff

---
+++ Brains Eden 2019/Assets/Scripts/Tutorial_System.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_System : MonoBehaviour
+{
+    [SerializeField] ChainManager chain_man;
+
+    enum States {BLANK,STICK_ENEMIES,STICK_BOMBS,DAMAGE_SPELLS,END_MESSAGE,START }
+    States current_state = States.BLANK;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(current_state)
+        {
+            case States.BLANK:
+                {
+
+                    break;
+                }
+            case States.STICK_ENEMIES:
+                {
+
+                    break;
+                }
+            case States.STICK_BOMBS:
+                {
+
+                    break;
+                }
+            case States.DAMAGE_SPELLS:
+                {
+
+                    break;
+                }
+            case States.END_MESSAGE:
+                {
+
+                    break;
+                }
+            case States.START:
+                {
+
+                    break;
+                }
+        }
+    }
+}
