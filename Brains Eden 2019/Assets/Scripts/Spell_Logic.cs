60fa86034382daee73d212a097b116f840eb1a29
Sam <chandler27s@yahoo.com>
2019-06-29 09:52:05 +0100

    Removed testing terrains for spell hovering

--- Brains Eden 2019/Assets/Scripts/Spell_Logic.cs
+++ Brains Eden 2019/Assets/Scripts/Spell_Logic.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spell_Logic : MonoBehaviour
 {
+    [SerializeField] private float hover_height; // Height the spell projectile will hover off of the floor
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,13 @@ public class Spell_Logic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit = new RaycastHit();
+        Physics.Raycast(transform.position, Vector3.down, out hit);
 
+        if (hit.collider != null)
+        {
+            transform.position = hit.point + (Vector3.up * hover_height);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
