f05a80a7f60368798137821e6fcb07a5e2128301
Sam <chandler27s@yahoo.com>
2019-06-30 15:42:36 +0100

    Set spawn interval properly and updated menu controls

--- Brains Eden 2019/Assets/Scripts/Camera_Boom.cs
+++ Brains Eden 2019/Assets/Scripts/Camera_Boom.cs
@@ -9,6 +9,7 @@ public class Camera_Boom : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
--- Brains Eden 2019/Assets/Scripts/Menu_Systems/Main_Menu_Control.cs
+++ Brains Eden 2019/Assets/Scripts/Menu_Systems/Main_Menu_Control.cs
@@ -10,6 +10,9 @@ public class Main_Menu_Control : MonoBehaviour
     [SerializeField] Button scores_button;
     [SerializeField] Button quit_button;
 
+    [SerializeField] GameObject highscore_board_obj;
+    [SerializeField] Button back_button;
+
     [SerializeField] highscore_show score_shower;
 
     [SerializeField] string game_scene_name;
@@ -24,6 +27,7 @@ public class Main_Menu_Control : MonoBehaviour
         start_button.onClick.AddListener(Start_Clicked);
         scores_button.onClick.AddListener(Scores_Clicked);
         quit_button.onClick.AddListener(Quit_Clicked);
+        back_button.onClick.AddListener(Back_Clicked);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
@@ -48,7 +52,11 @@ public class Main_Menu_Control : MonoBehaviour
     }
     void Scores_Clicked()
     {
-        score_shower.are_scores_shown = true;
+        event_sys.SetSelectedGameObject(back_button.gameObject);
+    }
+    void Back_Clicked()
+    {
+        event_sys.SetSelectedGameObject(scores_button.gameObject);
     }
     void Quit_Clicked()
     {
