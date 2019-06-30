14438302c8c17e493106b0dc0bb54c5eb797f45a
Chris <hargrove.chris91@gmail.com>
2019-06-30 14:30:15 +0100

    Added Sound And Updated Menu

---
+++ Brains Eden 2019/Assets/Scripts/DontDestroyOnLoadScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoadScript : MonoBehaviour
+{
+    public GameObject SoundManagerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiation();
+    }
+
+    private void Instantiation()
+    {
+        GameObject SoundManagerInstance = GameObject.FindGameObjectWithTag("SoundManager");
+        GameObject SoundManager;
+
+        if (SoundManagerInstance == null) {
+            SoundManager = Instantiate(SoundManagerPrefab);
+        }
+        else {
+            SoundManager = SoundManagerInstance;
+        }
+    }
+}
---
+++ Brains Eden 2019/Assets/Sound/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer Mixer;
+    public AudioSource Source;
+    public AudioClip Music;
+
+    // Start is called before the first frame update
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+
+        Source.clip = Music;
+        Source.Play();
+    }
+    
+}
