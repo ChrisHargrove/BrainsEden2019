4ddcbceef88f0223ac33636c9415fd6a87feb27d
27chandler <chandler27s@yahoo.com>
2019-07-04 20:30:32 +0100

    Created tutorial arrows to help convey the tutorial
    I created a pointer arrow prefab and script. The arrows point from one gremil to another to indicate to stick them together. They can be re-purposed for other uses, but are currently in the tutorial zone.

---
+++ Brains Eden 2019/Assets/Scripts/Pointing_Indicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointing_Indicator : MonoBehaviour
+{
+    [SerializeField] private Transform point_from;
+    [SerializeField] private Transform point_to;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((point_from != null) && (point_to != null))
+        {
+            transform.position = point_from.position;
+            transform.LookAt(point_to,Vector3.up);
+        }
+    }
+}
